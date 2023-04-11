f5dd957e9980910d7c156695daeb1081561290bb
Franz Suarez Ramirez <>
2023-04-11 18:47:35 -0500

    Refactorizando código y creando adaptadores de HTTP

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,21 @@
-import axios, { AxiosInstance } from "axios"
 import { Injectable } from "@nestjs/common"
 import { PokeResponse } from "./interfaces/poke-response.interface"
 import { PokemonService } from "src/pokemon/pokemon.service"
+import { AxiosAdapter } from "src/common/adapters/axios.adapter"
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly pokemonService: PokemonService) {}
-  private readonly axios: AxiosInstance = axios
+  constructor(
+    private readonly pokemonService: PokemonService,
+    private readonly http: AxiosAdapter,
+  ) {}
 
   async executeSeed() {
     //Limpiamos la data de la BD
     this.pokemonService.clearPokemonCollection()
 
     // Armamos el request para insertar en BD
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       `https://pokeapi.co/api/v2/pokemon?limit=150`,
     )
 
